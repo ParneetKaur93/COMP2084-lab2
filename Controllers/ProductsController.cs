8d1a3fe06f49b115f7f3df7d95a6d38de41c06fd
ParneetKaur <200428731@student.georgianc.on.ca>
2020-01-29 15:15:04 -0500

    lab 2 completed all

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            string[] products = { "Laptop", "Desktop", "Tablet" };
+            ViewData["products"] = products;
+             return View();
+        }
+
+        public string details(string product)
+        {
+            string message = "here is your product" + product;
+                return message;
+        }
+    }
+}
